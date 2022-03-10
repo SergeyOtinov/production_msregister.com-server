90566b8f48143b5e565c38af3304a7f119fd837e
SerhiiOtinov <60657116+SerhiiOtinov@users.noreply.github.com>
2022-03-10 13:53:06 +0200

    11/02/2022_ver.1.01

--- controllers/userController.js
+++ controllers/userController.js
@@ -110,8 +110,8 @@ class UserController {
 	async requestElma(req, res, next) {
 		try {
 			const { requestBody } = req.body;
-			// const { user_email } = requestBody;
-			console.log(requestBody.context)
+			const { user_email } = requestBody.context;
+			console.log(user_email)
 			// const response = mailerService.send(user_email)
 			// const response = userService.sendRequestElma(requestBody)
 			// if (response) {
